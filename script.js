442b17fd81d15576596be3c9bdcc09c70ad6a64a
German Caicedo <german.caicedoa1@gmail.com>
2023-01-21 01:11:18 -0500

    Prettier

--- script.js
+++ script.js
@@ -2,25 +2,27 @@ const block = document.getElementById("block");
 const hole = document.getElementById("hole");
 const character = document.getElementById("character");
 let jumping = 0;
-var counter = 0;
+let counter = 0;
 
 hole.addEventListener('animationiteration', () => {
   let random = -((Math.random() * 300) + 150);
   hole.style.top = random + "px";
   counter++;
 })
-// Gravedad
-setInterval(function () {
+
+setInterval(() => {
   let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
   let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-  let cTop = -(500-characterTop);
+  let cTop = -(640 - characterTop);
+
   if (jumping == 0) {
-    character.style.top = (characterTop + 3) + "px";
+    // Gravedad
+    character.style.top = (characterTop + 4) + "px";
   }
   // Dead
-  if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
-    alert("game over. Score:"+ counter);
+  if ((characterTop > 640) || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 140)))) {
+    // alert("game over. Score:"+ counter);
     character.style.top = 100 + "px"
     counter = 0;
   }
@@ -29,9 +31,9 @@ setInterval(function () {
 const jump = () => {
   jumping = 1;
   let jumpCount = 0;
-  let jumpInterval = setInterval(function () {
+  let jumpInterval = setInterval(() => {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    if(characterTop>6){
+    if (characterTop > 6) {
       //Jump height
       character.style.top = (characterTop - 5) + "px";
     }
@@ -45,7 +47,7 @@ const jump = () => {
   })
 }
 
-setInterval(function(){
+setInterval(function () {
   const score = document.getElementById("score");
   score.innerHTML = `Score: ${counter}`;
 }, 2)
